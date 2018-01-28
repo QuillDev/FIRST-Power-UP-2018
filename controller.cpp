83653e6473d966e01094c2d31cc4fcccef7c09ee
Robert Brunson <siinspam@gmail.com>
2018-01-28 15:23:11 -0500

    merged to main.cpp

--- controller.cpp
+++ controller.cpp
@@ -14,8 +14,4 @@ Joystick *xboxCon
                 logic = xboxCon.getXButton();
                 value = xboxCon.getX(); //retrieves how far the X of the stick is
                 value = xboxCon.getY(); //retrieves how far the Y of the stick is
-                /**please note that the ()'s are for inserting robot functions when we get to that point
-                
-                //Note, these are temporary labels and WILL MOST LIEKLY CHANGE when we
-                Find the libraries of the controller we're using.**/
             }
