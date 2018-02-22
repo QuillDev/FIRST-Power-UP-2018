7227280b950bc808aa717b2dd3dd1f35c898b3a8
Quill <siinspam@gmail.com>
2018-02-22 13:11:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,35 +20,21 @@ public:
 		frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 		left = new frc::Spark(0);
 		right = new frc::Spark(1);
+		intake = new frc::Spark(2);
+		swing = new frc::Spark(3);
 	}
 //Create pointers (*) for left and right
 	frc::Spark *left;
 	frc::Spark *right;
 	frc::Spark *intake;
-	frc::Spark *arm;
+	frc::Spark *swing;
 	frc::Joystick *stick;
-	double lx, ly, rx, ry, ly1, ry1, i, h, val1, val2, rbump, lbump, lbumpx, lbumpy, intakey, arm1, arm2 = 0;
-	double deadzone = 0.0;
+	double ly, ry, deadzone, i, h, val1, val2, rbump, lbump, lbumpx, lbumpy, intakey, arm1, arm2 = 0;
 	double inc = .005;
 
-	/*
-	 * This autonomous (along with the chooser code above) shows how to
-	 * select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * GetString line to get the auto name from the text box below the Gyro.
-	 *
-	 * You can add additional auto modes by adding additional comparisons to
-	 * the
-	 * if-else structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as
-	 * well.
-	 */
 	void AutonomousInit() override {
 		m_autoSelected = m_chooser.GetSelected();
-		// m_autoSelected = SmartDashboard::GetString(
-		// 		"Auto Selector", kAutoNameDefault);
+
 		std::cout << "Auto selected: " << m_autoSelected << std::endl;
 
 		if (m_autoSelected == kAutoNameCustom) {
@@ -109,26 +95,25 @@ public:
 		if(lbump == true){
 			left->Set(-(.5 + (lbumpx * inc)));
 			right->Set(.55 + (lbumpx * inc));
-			stick->SetRumble(stick->kLeftRumble, .7);
 			lbumpx++;
 		} else {lbumpx = 0;}
 
 		if(rbump == true){
 			left->Set(.5 + (lbumpy * inc));
 			right->Set(-(.5 + (lbumpy * inc)));
-			stick->SetRumble(stick->kRightRumble, .7);
 			lbumpy++;
 		} else{lbumpy = 0;}
 
 		if(std::abs(intakey) > 0){
 			intake->Set(-1);
 		}
+
 		if(arm1 == true){
-			arm->Set(.3);
+			swing->Set(.3);
 		}
 
 		if(arm2 == true){
-			arm ->Set(-.5);
+			swing ->Set(-.5);
 		}
 
 	void TestPeriodic() {}
