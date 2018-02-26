e78cc803b7e28406fdefe555541bde313386b8cb
Quill <siinspam@gmail.com>
2018-02-26 10:52:54 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-//Robot.cpp
+#include <boltbeard.h>
 #include <iostream>
 #include <string>
 
@@ -11,31 +11,50 @@
 #include <Spark.h>
 #include <Joystick.h>
 #include <Math.h>
-#include <WPILib.h>
 
-class Robot : public frc::IterativeRobot {
+//custom headers
+#include "boltbeard.h" //I hope you're satisfied @StripedMonkey
+
+using namespace std;
+using namespace frc;
+
+class Robot : public IterativeRobot {
 public:
 	void RobotInit() {
 		m_chooser.AddDefault(kAutoNameDefault, kAutoNameDefault);
 		m_chooser.AddObject(kAutoNameCustom, kAutoNameCustom);
-		frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
-		left = new frc::Spark(0);
-		right = new frc::Spark(1);
-		intake = new frc::Spark(2);
-		swing = new frc::Spark(3);
+		SmartDashboard::PutData("Auto Modes", &m_chooser);
+
+		//Create Sparks
+		left = new Spark(0);
+		right = new Spark(1);
+		intake1 = new Spark(2);
+		intake2 = new Spark(3);
+		swing = new Spark(4);
+
 	}
-//Create pointers (*) for left and right
-	frc::Spark *left;
-	frc::Spark *right;
-	frc::Spark *intake;
-	frc::Spark *swing;
-	frc::Joystick *stick;
-	double ly, ry, deadzone, i, h, val1, val2, rbump, lbump, lbumpx, lbumpy, intakey, arm1, arm2 = 0;
-	double inc = .005;
+	//Pointers for Sparks
+	Spark *left;
+	Spark *right;
+	Spark *intake1;
+	Spark *intake2;
+	Spark *swing;
+	Joystick *stick;
+
+	//declare doubles
+	double ly, ls,lbs, ry, rs, rbs = 0;
+	double deadzone = .1;
+
+	//declare ints
+	int i, h, m, n = 0;
+
+	//declare booleans
+	bool lb, rb;
 
 	void AutonomousInit() override {
 		m_autoSelected = m_chooser.GetSelected();
-
+		// m_autoSelected = SmartDashboard::GetString(
+		// 		"Auto Selector", kAutoNameDefault);
 		std::cout << "Auto selected: " << m_autoSelected << std::endl;
 
 		if (m_autoSelected == kAutoNameCustom) {
@@ -54,77 +73,58 @@ public:
 	}
 
 	void TeleopInit() {
-		stick = new frc::Joystick(0);
+		stick = new Joystick(0);
 	}
 
 	void TeleopPeriodic() {
+		//sticks
 		ly = stick->GetRawAxis(1);
 		ry = stick->GetRawAxis(5);
-		rbump = stick->GetRawButton(6);
-		lbump = stick->GetRawButton(5);
-		intakey = stick->GetRawAxis(3);
-		arm1 = stick->GetRawButton(1);
-		arm2 = stick->GetRawButton(2);
+		ls = spd(i, -1);
+		rs = spd(h, 1);
 
-		deadzone = .2;
+		//bumpers
+		lb = stick->GetRawButton(5);
+		rb = stick->GetRawButton(6);
+		lbs = spd(m, 1);
+		rbs = spd(n, 1);
 
-		if(std::abs(ly) > deadzone){
-			val1 = .5 + (inc * i);
-			if(ly > 0){left->Set(-val1);}
-			else{left->Set(val1+.05);}
+		//stick logic
+		if(abs(ly) > deadzone && abs(ls) < 1){
+			left ->Set(ls);
 			i++;
-			}
+		} else if(abs(ls) >= 1){if(abs(ls) > 0){left->Set(-1);}else{left->Set(1);}}
+		else{left->Set(0);i=0;}
 
-		else{
-			left->Set(0);
-			ly1 = 0.0;
-			i = 0;
-		}
-
-		if(std::abs(ry) > deadzone){
-			val2 = .5 + (inc * h);
-			if(ry > 0){right->Set(val2);}
-			else{right->Set(-val2);}
+		if(ry > deadzone && abs(rs) < 1){
+			right->Set(rs);
 			h++;
-			}
+		} else if(abs(rs) >= 1){if(abs(rs) > 0){right->Set(1);}else{right->Set(-1);}}
+		else{right->Set(0);h=0;}
 
-		else{
-			right->Set(0);
-			h = 0;
+		//bumper logic
+		if(lb == true && abs(lbs) < 1 && abs(ly) < deadzone){
+			left->Set(-lbs);
+			right->Set(lbs);
+			m++;
+
+		} else if(abs(lbs) >= 1 && abs(ly) < deadzone){left->Set(-1); right->Set(1);}
+		else if(abs(lbs) < deadzone){left->Set(0); right->Set(0);m=0;}
+		
+		if(rb == true && abs(ry) < deadzone && abs(ly) < 1){
+			
 		}
+	}
 
-		if(lbump == true){
-			left->Set(-(.5 + (lbumpx * inc)));
-			right->Set(.55 + (lbumpx * inc));
-			lbumpx++;
-		} else {lbumpx = 0;}
-
-		if(rbump == true){
-			left->Set(.5 + (lbumpy * inc));
-			right->Set(-(.5 + (lbumpy * inc)));
-			lbumpy++;
-		} else{lbumpy = 0;}
-
-		if(std::abs(intakey) > 0){
-			intake->Set(-1);
-		}
-
-		if(arm1 == true){
-			swing->Set(.3);
-		}
-
-		if(arm2 == true){
-			swing ->Set(-.5);
-		}
 
 	void TestPeriodic() {}
 
 private:
-	frc::LiveWindow& m_lw = *LiveWindow::GetInstance();
-	frc::SendableChooser<std::string> m_chooser;
-	const std::string kAutoNameDefault = "Default";
-	const std::string kAutoNameCustom = "My Auto";
-	std::string m_autoSelected;
+	LiveWindow& m_lw = *LiveWindow::GetInstance();
+	SendableChooser<string> m_chooser;
+	const string kAutoNameDefault = "Default";
+	const string kAutoNameCustom = "My Auto";
+	string m_autoSelected;
 };
 
 START_ROBOT_CLASS(Robot)
